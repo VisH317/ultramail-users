b1e672974f95366a505a35f36aa7a2b3a2920488
Vis <vt201916384@gmail.com>
2023-05-08 23:52:33 -0400

    handlers setup

---
+++ src/UltraMail.ts
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export default class UltraMail {
+    private key: string
+
+    constructor(key: string) {
+        this.key = key
+    }
+
+    public createUser() {}
+    public addUserAction() {}
+    public deleteUser() {}
+}
