d02fc5f5b2d3aa7b64c8c3f4690ea6b46587c9a4
Vis <vt201916384@gmail.com>
2023-05-10 22:33:53 -0400

    testing stuff

--- tests/UltraMail.test.ts
+++ tests/UltraMail.test.ts
@@ -4,12 +4,14 @@ const randomKey = "aoweijfaskjvnasda"
 
 test("testing user route online", async () => {
     const ult = new UltraMail(randomKey)
+    console.log("ORIGIN: ", process.env.ORIGIN)
     const res = await ult.testUsers()
     expect(res).toBe(true)
 })
 
 test("testing addUserAction route online", async () => {
     const ult = new UltraMail(randomKey)
+    console.log("ORIGIN: ", process.env.ORIGIN)
     const res = await ult.testAdd()
     expect(res).toBe(true)
 })
